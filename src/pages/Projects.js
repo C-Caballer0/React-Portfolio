7f943e45945ecfd68b6b31b396bc54556bd951c5
Carlo Caballero <carlopcaballero@gmail.com>
2023-04-03 15:48:42 -0400

    Added new projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -8,6 +8,11 @@ const Projects = () => {
         <h2>Cask Music</h2>
         <img src={ require('../assets/CASKMusic.PNG') } />
         <p>An application that searches for songs or artists via various music provider APIs</p>
+    </div>
+    <div className="projectCard">
+        <h2>RecipePop</h2>
+        <img src={ require('../assets/RecipePOP.PNG') } />
+        <p>An in-progress application allows for search for and sharing recipes and allows basic social media capabilities</p>
     </div>    
     </div>
     </>
