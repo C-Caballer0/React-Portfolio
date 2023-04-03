5892baea422cac046003e7fa13e23516ed7ae845
Carlo Caballero <carlopcaballero@gmail.com>
2023-04-03 15:42:54 -0400

    Added project template

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,7 +5,9 @@ const Projects = () => {
     <div className="content">
     <h1>Some samples of my work:</h1>
     <div className="projectCard">
-        
+        <h2>Cask Music</h2>
+        <img src={ require('../assets/CASKMusic.PNG') } />
+        <p>An application that searches for songs or artists via various music provider APIs</p>
     </div>    
     </div>
     </>
